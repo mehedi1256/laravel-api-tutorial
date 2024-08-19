283158e1d3429b4e860699bc6e2eb19ca45f2d7c
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-08-19 08:28:04 +0600

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login() {
-        return $this->ok('login successful');
+
+    public function login(ApiLoginRequest $request) {
+        return $this->ok($request->get('email'));
     }
 }
